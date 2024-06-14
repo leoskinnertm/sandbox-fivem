a2626c195f9d4e15b27f573b9e5c4f41c313a820
BlazerBoccle <152270436+BlazerBoccle@users.noreply.github.com>
2024-06-14 19:55:14 +0100

    some maps

---
+++ server/resources/[main]/[custom-maps]/Dolu_Last_Train/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version 'adamant'
+games { 'gta5' }
+this_is_a_map 'yes'
+
+-- $$$$$$$\   $$$$$$\  $$\       $$\   $$\ 
+-- $$  __$$\ $$  __$$\ $$ |      $$ |  $$ |
+-- $$ |  $$ |$$ /  $$ |$$ |      $$ |  $$ |
+-- $$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |
+-- $$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |
+-- $$ |  $$ |$$ |  $$ |$$ |      $$ |  $$ |
+-- $$$$$$$  | $$$$$$  |$$$$$$$$\ \$$$$$$  |
+-- \_______/  \______/ \________| \______/ 
+
+description "Licensed under the Dolu License, you may not use this file except in compliance with the License. A copy of the license is available in each packages purchased on https://dolu.tebex.io/"
+made_by 'Dolu & Synced_Moertn'
+
+
+data_file 'TIMECYCLEMOD_FILE' 'dolu_lasttrain_timecycle.xml'
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+data_file 'SCALEFORM_DLC_FILE' 'stream/[minimap]/int2954770676.gfx'
+
+files {
+         "dolu_lasttrain_timecycle.xml",
+ 		"interiorproxies.meta",
+}
+
+
+-- Make sure to subscribe to our twitch channels
+-- https://www.twitch.tv/SyncedGamingTV
+-- https://www.twitch.tv/DoluTattoo
---
+++ server/resources/[main]/[custom-maps]/e49_dgt_den/fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'bodacious'
+games { 'gta5' }
+this_is_a_map 'yes'
+
+data_file "DLC_ITYP_REQUEST" "stream/e49_dgt_den_props_shared.ytyp"
