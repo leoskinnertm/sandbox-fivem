b3b4773a308743b0a6ba139e6790e01257d775f8
BlazerBoccle <152270436+BlazerBoccle@users.noreply.github.com>
2024-06-14 17:55:01 +0100

    Free icons on admin

--- server/resources/[sandbox]/sandbox-admin/ui/src/containers/App/index.jsx
+++ server/resources/[sandbox]/sandbox-admin/ui/src/containers/App/index.jsx
@@ -10,8 +10,8 @@ import {
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/pro-solid-svg-icons';
-import { far } from '@fortawesome/pro-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { HashRouter } from 'react-router-dom';
 
