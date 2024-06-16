3e14ac6ce0453505325876f9c7e2572c33d699f7
BlazerBoccle <152270436+BlazerBoccle@users.noreply.github.com>
2024-06-16 20:04:09 +0100

    file clean up

--- server/resources/[sandbox]/sandbox-admin/fxmanifest.lua
+++ server/resources/[sandbox]/sandbox-admin/fxmanifest.lua
@@ -1,21 +1,14 @@
-server_script "35DG3.lua"
-client_script "35DG3.lua"
 fx_version("cerulean")
 game("gta5")
 lua54("yes")
 client_script("@sandbox-base/components/cl_error.lua")
 client_script("@sandbox-pwnzor/client/check.lua")
 
--- shared_scripts {
---     'config/*.lua'
--- }
 
 client_scripts({
 	"client/client.lua",
 	"client/attach.lua",
 	"client/noclip/*.lua",
-	-- 'client/menu.lua',
-	-- 'client/shitty_menu.lua',
 	"client/nui.lua",
 	"client/ids.lua",
 	"client/nuke.lua",
