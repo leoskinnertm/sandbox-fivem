090761f596a0059bcdb7f7937eb6945c668fc69f
leoskinner <leoskinner34@gmail.com>
2024-06-28 22:39:34 +0100

    Removed old phoenix anticheat and fixed exporter icon

--- server/resources/[cfx-default]/[gamemodes]/basic-gamemode/fxmanifest.lua
+++ server/resources/[cfx-default]/[gamemodes]/basic-gamemode/fxmanifest.lua
@@ -1,5 +1,3 @@
-server_script "5V.lua"
-client_script "5V.lua"
 -- This resource is part of the default Cfx.re asset pack (cfx-server-data)
 -- Altering or recreating for local use only is strongly discouraged.
 
