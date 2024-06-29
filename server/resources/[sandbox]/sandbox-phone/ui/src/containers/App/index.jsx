304e6ce2588bbb35fc2f07e179fb5e6e12e59189
Mysticz <152270436+MysticzDev@users.noreply.github.com>
2024-06-29 22:18:33 +0100

    fix sandbox wallpaper

--- server/resources/[sandbox]/sandbox-phone/ui/src/containers/App/index.jsx
+++ server/resources/[sandbox]/sandbox-phone/ui/src/containers/App/index.jsx
@@ -7,9 +7,10 @@ import {
 	StyledEngineProvider,
 } from '@mui/material';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/pro-solid-svg-icons';
+import { far } from '@fortawesome/pro-regular-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fad } from '@fortawesome/pro-duotone-svg-icons';
 import { HashRouter as Router } from 'react-router-dom';
 import chroma from 'chroma-js';
 
